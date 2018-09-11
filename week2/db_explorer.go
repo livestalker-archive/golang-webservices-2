8faa6d195714cde512be4e4b182f307bde2731a1
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-09-11 11:20:59 +0300

    Working

--- week2/db_explorer.go
+++ week2/db_explorer.go
@@ -1,4 +1,72 @@
 package main
 
-// тут вы пишете код
-// обращаю ваше внимание - в этом задании запрещены глобальные переменные
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+)
+
+type ExplorerSvc struct {
+	db          *sql.DB
+	Tables      []*DBTable
+	TablesNames map[string]*DBTable
+}
+
+type DBTable struct {
+	Name string
+}
+
+func (s *ExplorerSvc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		s.ListAllTables(w, r)
+		return
+	}
+	s.TableRouter(w, r)
+}
+
+func NewDbExplorer(db *sql.DB) (http.Handler, error) {
+	svc := &ExplorerSvc{
+		db: db,
+	}
+	svc.FillDBMeta()
+	http.Handle("/", svc)
+	return svc, nil
+}
+
+func (s *ExplorerSvc) FillDBMeta() {
+	tables := make([]*DBTable, 0)
+	tablesNames := make(map[string]*DBTable)
+	rows, _ := s.db.Query("SHOW TABLES")
+	for rows.Next() {
+		tableName := ""
+		err := rows.Scan(&tableName)
+		if err != nil {
+			panic(err)
+		}
+		t := &DBTable{Name: tableName}
+		tables = append(tables, t)
+		tablesNames[tableName] = t
+		s.Tables = tables
+		s.TablesNames = tablesNames
+	}
+}
+
+func (s *ExplorerSvc) ListAllTables(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	res := make(map[string]map[string]interface{})
+	tables := make([]string, 0)
+	for _, el := range s.Tables {
+		tables = append(tables, el.Name)
+	}
+	res["response"] = make(map[string]interface{})
+	res["response"]["tables"] = tables
+	body, _ := json.Marshal(res)
+	w.Write(body)
+}
+
+func (s *ExplorerSvc) TableRouter(w http.ResponseWriter, r *http.Request) {
+
+}
