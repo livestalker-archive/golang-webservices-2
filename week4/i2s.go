c97043fe458713d1b395b2fdfa7c740856908eac
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-10-03 17:10:41 +0300

    Week4

--- week4/i2s.go
+++ week4/i2s.go
@@ -12,13 +12,15 @@ func i2s(data interface{}, out interface{}) error {
 	}
 	st := reflect.TypeOf(out).Elem()
 	sv := reflect.ValueOf(out).Elem()
-	fmt.Println(sv)
 	n := st.NumField()
 	fillStruct(st, sv, n, data, out)
 	return nil
 }
 
-func fillStruct(st reflect.Type, sv reflect.Value, n int, data interface{}, out interface{}) {
+func fillStruct(st reflect.Type, sv reflect.Value, n int, data interface{}, out interface{}) reflect.Value {
+	if sv.Type().Kind() == reflect.Ptr {
+		sv = sv.Elem()
+	}
 	for i := 0; i < n; i++ {
 		fName := st.Field(i).Name
 		if m, ok := data.(map[string]interface{}); ok {
@@ -30,8 +32,19 @@ func fillStruct(st reflect.Type, sv reflect.Value, n int, data interface{}, out 
 			case bool:
 				sv.Field(i).SetBool(v)
 			case interface{}:
-				fillStruct(st.Field(i).Type, sv.Field(i), st.Field(i).Type.NumField(), data, out)
+				switch nestedV := v.(type) {
+				case map[string]interface{}:
+					fillStruct(st.Field(i).Type, sv.Field(i), st.Field(i).Type.NumField(), m[fName], out)
+				case []interface{}:
+					for ix, _ := range nestedV {
+						newEl := reflect.New(st.Field(i).Type.Elem())
+						tmp := fillStruct(st.Field(i).Type.Elem(), newEl, newEl.Elem().NumField(), nestedV[ix], out)
+						sv.Field(i).Set(reflect.Append(sv.Field(i), tmp))
+						fmt.Println(sv)
+					}
+				}
 			}
 		}
 	}
+	return sv
 }
