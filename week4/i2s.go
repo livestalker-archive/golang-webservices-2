754aef08b7c79b18e0da0af9e3ac5b4274aadf97
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-10-03 23:30:02 +0300

    Horrible code. Need refactoring

--- week4/i2s.go
+++ week4/i2s.go
@@ -7,28 +7,43 @@ import (
 )
 
 func i2s(data interface{}, out interface{}) error {
-	if reflect.TypeOf(out).Elem().Kind() != reflect.Struct && reflect.TypeOf(out).Elem().Kind() != reflect.Slice {
-		return errors.New("out must be struct")
+	if reflect.TypeOf(out).Kind() != reflect.Ptr {
+		return errors.New("out must be pointer")
+	}
+	if reflect.TypeOf(data).Kind() != reflect.Map && reflect.TypeOf(out).Elem().Kind() != reflect.Struct {
+		err := checkTypes(reflect.TypeOf(data).Kind(), reflect.TypeOf(out).Elem().Kind())
+		if err != nil {
+			return err
+		}
+	}
+	if reflect.TypeOf(data).Kind() == reflect.Slice && reflect.TypeOf(out).Elem().Kind() == reflect.Struct {
+		return errors.New("Error")
 	}
 	st := reflect.TypeOf(out).Elem()
 	sv := reflect.ValueOf(out).Elem()
 	if reflect.TypeOf(out).Elem().Kind() == reflect.Struct {
 		n := st.NumField()
-		fillStructFromMap(st, sv, n, data, out)
+		_, err := fillStructFromMap(st, sv, n, data, out)
+		return err
 	}
 	if reflect.TypeOf(out).Elem().Kind() == reflect.Slice {
-		fillStructFromSlice(st, sv, data, out)
+		_, err := fillStructFromSlice(st, sv, data, out)
+		return err
 	}
 	return nil
 }
 
-func fillStructFromMap(st reflect.Type, sv reflect.Value, n int, data interface{}, out interface{}) reflect.Value {
+func fillStructFromMap(st reflect.Type, sv reflect.Value, n int, data interface{}, out interface{}) (reflect.Value, error) {
 	if sv.Type().Kind() == reflect.Ptr {
 		sv = sv.Elem()
 	}
 	for i := 0; i < n; i++ {
 		fName := st.Field(i).Name
 		if m, ok := data.(map[string]interface{}); ok {
+			err := checkTypes(st.Field(i).Type.Kind(), reflect.TypeOf(m[fName]).Kind())
+			if err != nil {
+				return reflect.ValueOf(0), err
+			}
 			switch v := m[fName].(type) {
 			case string:
 				sv.Field(i).SetString(v)
@@ -43,22 +58,35 @@ func fillStructFromMap(st reflect.Type, sv reflect.Value, n int, data interface{
 				case []interface{}:
 					for ix, _ := range nestedV {
 						newEl := reflect.New(st.Field(i).Type.Elem())
-						tmp := fillStructFromMap(st.Field(i).Type.Elem(), newEl, newEl.Elem().NumField(), nestedV[ix], out)
+						tmp, _ := fillStructFromMap(st.Field(i).Type.Elem(), newEl, newEl.Elem().NumField(), nestedV[ix], out)
 						sv.Field(i).Set(reflect.Append(sv.Field(i), tmp))
 					}
 				}
 			}
 		}
 	}
-	return sv
+	return sv, nil
 }
 
-func fillStructFromSlice(st reflect.Type, sv reflect.Value, data interface{}, out interface{}) reflect.Value {
+func fillStructFromSlice(st reflect.Type, sv reflect.Value, data interface{}, out interface{}) (reflect.Value, error) {
 	for ix, _ := range data.([]interface{}) {
 		newEl := reflect.New(st.Elem())
-		tmp := fillStructFromMap(st.Elem(), newEl, newEl.Elem().NumField(), data.([]interface{})[ix], out)
+		tmp, _ := fillStructFromMap(st.Elem(), newEl, newEl.Elem().NumField(), data.([]interface{})[ix], out)
 		sv.Set(reflect.Append(sv, tmp))
 		fmt.Println(sv)
 	}
-	return reflect.ValueOf(1)
+	return reflect.ValueOf(0), nil
+}
+
+func checkTypes(t1 reflect.Kind, t2 reflect.Kind) error {
+	if t1 == reflect.Int && t2 == reflect.Float64 {
+		return nil
+	}
+	if t1 == reflect.Struct && t2 == reflect.Map {
+		return nil
+	}
+	if t1 != t2 {
+		return errors.New("Error types")
+	}
+	return nil
 }
